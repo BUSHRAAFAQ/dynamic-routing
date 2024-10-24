dc828e01f3fd18f24d281ad9013267de2c0916ed
Bushra Afaq <165610940+BUSHRS@users.noreply.github.com>
2024-10-25 00:15:01 +0500

    first commit

---
+++ app/country/[country_name]/page.tsx
@@ -0,0 +1,55 @@
+export default function CountryName({params}:any){
+  const countries: { //arry of multiples objects of countries
+    name:string,
+    population :number,
+    capital:string,
+  }[]=[
+    {
+        name:"Pakistan",
+        population :21876005,
+        capital:"Islamabad",
+    },
+    {
+        name:"Qatar",
+        population :2785940,
+        capital:"Doha",
+    },
+    {
+        name:"Kuwait",
+        population :218675,
+        capital:"Kuwaitcity",
+    },
+    {
+        name:"Bahrain",
+        population :187590,
+        capital:"Manama",
+    },
+    {
+        name:"Dobai",
+        population :45678,
+        capital:"Abu Dahbi",
+    },
+  ];
+  function findCountry(country_url:string){
+   return countries.find(country=>country.name.toLowerCase()==country_url.toLocaleLowerCase())
+  }
+
+  let result = findCountry(params.country_name);
+ return(
+        <div>
+            {
+                result ?(
+                    <>
+                      <h1 >Country Name:{result.name}</h1>
+            <h1>Country Capital:{result.capital}</h1>
+            <h1>Country Population:{result.population}</h1>
+                    </>
+                ):(
+                    <h1>{params.country_name}Country Not Found</h1>
+                )
+            }
+        
+        </div>
+
+    )
+}
---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+
+import Image from "next/image";
+import  Styles from "./page.module.css" ;
+import Link from "next/link";
+
+export default function Home(){
+  return(
+    <div>
+      <h1>Country List</h1>
+      <Link href="/country/pakistan" style={{margin:"10px", borderRadius:"10px",border:"5px solid purple"
+        ,padding:"10px" }}>Pakistan</Link>
+      <Link href="/country/india"style={{margin:"10px",borderRadius:"10px",border:"5px solid black"
+        ,padding:"10px" }}>Qatar</Link>
+         <Link href="/country/india"style={{margin:"10px",borderRadius:"10px",border:"5px solid blue"
+        ,padding:"10px" }}>Kuwait</Link>
+         <Link href="/country/india"style={{margin:"10px",borderRadius:"10px",border:"5px solid yellow"
+        ,padding:"10px" }}>Bahrain</Link>
+         <Link href="/country/india"style={{margin:"10px",borderRadius:"10px",border:"5px solid pink"
+        ,padding:"10px" }}>Dobai</Link>
+    </div>
+
+  )
+}
